6a4521e0127077696ff18b5160efe3bdf8ea35f7
luvia <luviaamagaali07@gmail.com>
2026-02-03 17:10:00 -0600

    Ajustar roles de base de datos

--- db/roles.sql
+++ db/roles.sql
@@ -1,20 +1,24 @@
 DO $$
+DECLARE
+	app_user text := '__DB_APP_USER__';
+	app_password text := '__DB_APP_PASSWORD__';
+	db_name text := '__POSTGRES_DB__';
 BEGIN
-	IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app_user') THEN
-		CREATE ROLE app_user LOGIN PASSWORD 'app_password';
+	IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = app_user) THEN
+		EXECUTE format('CREATE ROLE %I LOGIN PASSWORD %L', app_user, app_password);
 	END IF;
+
+	EXECUTE format('GRANT CONNECT ON DATABASE %I TO %I', db_name, app_user);
+	EXECUTE format('GRANT USAGE ON SCHEMA public TO %I', app_user);
+
+	EXECUTE format('REVOKE ALL ON ALL TABLES IN SCHEMA public FROM %I', app_user);
+	EXECUTE format('REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM %I', app_user);
+
+	EXECUTE format('GRANT SELECT ON vw_course_performance TO %I', app_user);
+	EXECUTE format('GRANT SELECT ON vw_teacher_load TO %I', app_user);
+	EXECUTE format('GRANT SELECT ON vw_students_at_risk TO %I', app_user);
+	EXECUTE format('GRANT SELECT ON vw_attendance_by_group TO %I', app_user);
+	EXECUTE format('GRANT SELECT ON vw_rank_students TO %I', app_user);
+
+	EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM %I', app_user);
 END $$;
-
-GRANT CONNECT ON DATABASE awosdb TO app_user;
-GRANT USAGE ON SCHEMA public TO app_user;
-
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM app_user;
-REVOKE ALL ON ALL SEQUENCES IN SCHEMA public FROM app_user;
-
-GRANT SELECT ON vw_course_performance TO app_user;
-GRANT SELECT ON vw_teacher_load TO app_user;
-GRANT SELECT ON vw_students_at_risk TO app_user;
-GRANT SELECT ON vw_attendance_by_group TO app_user;
-GRANT SELECT ON vw_rank_students TO app_user;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA public REVOKE ALL ON TABLES FROM app_user;
