e8e8186a0029098a904783938247f5762a60b82c
luvia <luviaamagaali07@gmail.com>
2026-02-11 23:26:24 -0600

    Separar backend con Server Actions

--- frontend/src/services/reports/index.ts
+++
@@ -1,5 +0,0 @@
-export { AttendanceService } from './attendance.service';
-export { StudentsService } from './students.service';
-export { CoursesService } from './courses.service';
-export { TeachersService } from './teachers.service';
-export type * from '../types';
